ec5c00744978807fb2b72c11e63d5bc6466181ff
tomasmaiorino <tomasmaiorino@gmail.com>
2017-01-13 11:51:42 -0200

    add new cross domain option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.allow_origins = "tomasmaiorino.github.io"
+  config.allow_origins = "tomasmaiorino.github.io http://tomasmaiorino.github.io"
 end
